b2c804f92bdaff7329cb81b33aadf3bac73f8f74
turquoise <monica.gosschalk@btinternet.com>
2016-02-13 14:06:28 +0000

    migrated the database

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Pinteresting
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    # required for heroku
+    # required for heroku for rails version 3
     config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #devise_for :users
+
   # get 'pages/home'
   get "about" => "pages#about"
 
